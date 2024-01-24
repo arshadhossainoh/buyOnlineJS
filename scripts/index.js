fc6e0a128044faf25aa1ac80f8462bb97fc1a3b8
arshadhossainoh <arshadhossain.oh@gmail.com>
2024-01-23 22:05:49 -0500

    worked on css aspect of the app

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,34 @@
-let itemsContainerEle = document.querySelector(".items-container");
+let bagItems = [];
 
-let innerHtml = "";
+displayItems();
 
-items.forEach((item) => {
-  innerHtml += `
-    <div class="item-container">
-    <img class="item-image" src= "${item.image}" alt="image">
-    <div class="rating">${item.rating.stars} ⭐| ${item.rating.count}</div>
-    <div class="company-name">${item.company}</div>
-    <div class="item-name">${item.item_name}</div>
-    <div class="price">
-      <span class="current-price">$ ${item.current_price}</span>
-      <span class="original-price">$ ${item.original_price}</span>
-      <span class="discount">(${item.discount_percentage}% OFF)</span>
-    </div>
-    <button class="btn-add-bag">Add to Bag</button>
-    </div>
-    
-    
-    `;
-});
+function addToBag(itemId) {
+  bagItems.push(itemId);
+}
 
-itemsContainerEle.innerHTML = innerHtml;
+function displayItems() {
+  let itemsContainerEle = document.querySelector(".items-container");
+
+  let innerHtml = "";
+
+  items.forEach((item) => {
+    innerHtml += `
+        <div class="item-container">
+        <img class="item-image" src= "${item.image}" alt="image">
+        <div class="rating">${item.rating.stars} ⭐| ${item.rating.count}</div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+          <span class="current-price">$ ${item.current_price}</span>
+          <span class="original-price">$ ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+        </div>
+        
+        
+        `;
+  });
+
+  itemsContainerEle.innerHTML = innerHtml;
+}
